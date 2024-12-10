70afa9f05680f76e793b7de71365a8bb0fde3db2
grngxd <36968271+grngxd@users.noreply.github.com>
2024-12-10 22:26:13 +0000

    [settings] preact components in react

--- packages/silica/core/discord/settings.tsx
+++ packages/silica/core/discord/settings.tsx
@@ -1,7 +1,7 @@
-import type { VNode } from "preact";
+import { type VNode, h } from "preact";
 import { getDispatcher } from "../flux/dispatcher";
 import { Patcher } from "../patcher";
-import { getFiberOwner } from "../react";
+import { getFiberOwner, renderPreactInReact } from "../react";
 import { getWebpackChunkByExports } from "../webpack";
 
 // IN VEIL THIS WHOLE FILE WOULDVE BEEN 140 LINES LONG
@@ -19,17 +19,19 @@ type CustomElement = {
     element?: () => VNode;
 };
 
-let p: Patcher;
+let p: Patcher | null = null;
+
 
 export const init = () => {
     getDispatcher()
         .waitForDispatch("USER_SETTINGS_MODAL_OPEN")
         .then(async () => {
+            console.log("Settings modal opened!");
             SettingsView = (getWebpackChunkByExports("ZP.prototype.getPredicateSections") as any).ZP
 
             p = new Patcher();
 
-            p.applyPatch(
+            p?.applyPatch(
                 SettingsView.prototype,
                 "getPredicateSections",
                 (originalMethod) => {
@@ -38,10 +40,11 @@ export const init = () => {
 
                         // TODO: actually add custom sections
                         sections.push({
-                            section: "custom",
-                            searchableTitles: ["Custom"],
-                            label: "Custom",
-                            ariaLabel: "Custom",
+                            section: "silica",
+                            searchableTitles: ["silica"],
+                            label: "silica",
+                            ariaLabel: "silica",
+                            element: () => renderPreactInReact(() => <h1>hi</h1>)
                         } as CustomElement);
                         return sections;
                     };
@@ -58,6 +61,6 @@ const rerenderSidebar = () => {
 }
 
 export const unload = () => {
-    p.removeAllPatches();
+    p?.removeAllPatches();
     rerenderSidebar();
 };
